3fe7db6620812172f6399837b63797d6c02ba7fe
martin.c <martin.c@freedom.press>
2026-02-10 11:25:14 -0500

    add client scan

--- .github/workflows/gd-scan.sh
+++ .github/workflows/gd-scan.sh
@@ -2,9 +2,9 @@
 scan=0
 mkdir gd-scans
 while read -r line; do
-        rm package/
+        rm -r package/
         archive=$(npm pack $line | tail -n1)
         tar -xzf $archive
-        guarddog --log-level debug npm verify package/ --output-format sarif >> gd-scans/guarddog-$scan.sarif
+        guarddog --log-level debug npm verify package/ --output-format sarif > gd-scans/guarddog-$scan.sarif
         ((scan++))
 done < package-list.txt
